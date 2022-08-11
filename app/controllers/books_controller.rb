23c7fb3ba03a5df525e02cce4ab27fcd2b02fb40
EC2 Default User <ec2-user@ip-172-31-17-129.ap-northeast-1.compute.internal>
2022-08-11 12:06:59 +0000

    [Fnish] 投稿機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,16 @@
 class BooksController < ApplicationController
   def new
+    @books = Book.new
+  end
+
+def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/top'
   end
 
   def index
+    @books = Book.all
   end
 
   def show
